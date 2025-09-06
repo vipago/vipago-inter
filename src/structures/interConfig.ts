f6dc8ad3d8930c2f27e54151867cb2c484904e92
Tiago Dinis <tiagodinis33@proton.me>
2025-09-06 21:25:53 +0100

    Update interConfig.ts

--- src/structures/interConfig.ts
+++ src/structures/interConfig.ts
@@ -25,39 +25,38 @@ export namespace InterConfig {
 		readonly sandbox: boolean;
 	}
 
-	export const make = (options: InterOptions) => {
-		return Layer.effect(
-			Tag,
-			Effect.gen(function* () {
-				const makeTLSConfig = (config: string | Buffer) => {
-					return Effect.gen(function* () {
-						if (typeof config === "string") {
-							const fs = yield* FileSystem;
+	export const makeEffect = (options: InterOptions) => {
+		return Effect.gen(function* () {
+			const makeTLSConfig = (config: string | Buffer) => {
+				return Effect.gen(function* () {
+					if (typeof config === "string") {
+						const fs = yield* FileSystem;
 
-							const file = yield* fs.readFile(config);
+						const file = yield* fs.readFile(config);
 
-							return Buffer.from(file);
-						}
+						return Buffer.from(file);
+					}
 
-						return config;
-					});
-				};
+					return config;
+				});
+			};
 
-				return {
-					certificate: Redacted.make(yield* makeTLSConfig(options.certificate)),
-					privKey: Redacted.make(yield* makeTLSConfig(options.priv_key)),
-					client_id: options.client_id,
-					client_secret: Redacted.make(options.client_secret),
-					base_url: options.base_url ?? (options.sandbox ? "https://cdpj-sandbox.partners.uatinter.co" : "https://cdpj.partners.bancointer.com.br"),
-					sandbox: Boolean(options.sandbox),
-					scope: options.scope,
-				};
-			}),
-		);
+			return {
+				certificate: Redacted.make(yield* makeTLSConfig(options.certificate)),
+				privKey: Redacted.make(yield* makeTLSConfig(options.priv_key)),
+				client_id: options.client_id,
+				client_secret: Redacted.make(options.client_secret),
+				base_url: options.base_url ?? (options.sandbox ? "https://cdpj-sandbox.partners.uatinter.co" : "https://cdpj.partners.bancointer.com.br"),
+				sandbox: Boolean(options.sandbox),
+				scope: options.scope,
+			};
+		});
 	};
-
+	export const make = (options: InterOptions) => {
+		return Layer.effect(Tag, makeEffect(options));
+	};
 	export const layerConfig = (config: Config.Config.Wrap<InterOptions>) => {
-		return pipe(Config.unwrap(config), Effect.map(make));
+		return Layer.effect(Tag, pipe(Config.unwrap(config), Effect.andThen(makeEffect)));
 	};
 
 	export const loadConfig = (config: InterOptions) => {
