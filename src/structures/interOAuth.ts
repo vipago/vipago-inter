7900ddadc3332a046cc3421cd5b06b40e4d5ba1d
Tiago Dinis <tiagodinis33@proton.me>
2026-03-06 11:55:45 +0000

    fix: passar o interconfig pra json pra usar como chave do cache

--- src/structures/interOAuth.ts
+++ src/structures/interOAuth.ts
@@ -4,22 +4,24 @@ import { InterOAuthResponseJSON } from "../models/oauth";
 import type { HttpsRequestError } from "./httpRequest";
 import { httpsRequestEffect } from "./httpRequest";
 import { InterConfig } from "./interConfig";
-
 const DEFAULT_ROUTE = "/oauth/v2/token";
-
-const accessTokenService = (config: InterConfig.InterConfig) =>
+export type UnredactedConfig = {
+  [K in keyof InterConfig.InterConfig]: InterConfig.InterConfig[K] extends Redacted.Redacted<infer U> ? U : InterConfig.InterConfig[K]
+} extends infer B ? B : never;
+const accessTokenService = (configJson: string) =>
 	Effect.gen(function* () {
+		const config = JSON.parse(configJson) as UnredactedConfig;
 		const response = yield* httpsRequestEffect(
 			"POST",
 			`${config.base_url}${DEFAULT_ROUTE}`,
 			new URLSearchParams({
 				client_id: config.client_id,
-				client_secret: Redacted.value(config.client_secret),
+				client_secret: config.client_secret,
 				grant_type: "client_credentials",
 				scope: typeof config.scope === "string" ? config.scope : config.scope.join(" "),
 			}).toString(),
-			Redacted.value(config.certificate),
-			Redacted.value(config.privKey),
+			config.certificate,
+			config.privKey,
 			{
 				"Content-Type": "application/x-www-form-urlencoded",
 			},
@@ -30,13 +32,13 @@ const accessTokenService = (config: InterConfig.InterConfig) =>
 export namespace InterCache {
 	export class Tag extends Effect.Tag("bancointer/InterCacheService")<
 		Tag,
-		Cache.Cache<InterConfig.InterConfig, Redacted.Redacted<string>, HttpsRequestError | ParseError>
+		Cache.Cache<string, Redacted.Redacted<string>, HttpsRequestError | ParseError>
 	>() {}
 
 	export const Layer = EffectLayer.effect(
 		Tag,
 		Cache.make({
-			capacity: 10,
+			capacity: Infinity,
 			timeToLive: "1 hour",
 			lookup: accessTokenService,
 		}),
@@ -46,5 +48,10 @@ export namespace InterCache {
 export const getGlobalOAuthToken = Effect.gen(function* () {
 	const config = yield* InterConfig.Tag;
 	const cache = yield* InterCache.Tag;
-	return yield* cache.get(config);
+	return yield* cache.get(JSON.stringify({
+		...config,
+		client_secret: Redacted.value(config.client_secret),
+		certificate: Redacted.value(config.certificate),
+		privKey: Redacted.value(config.privKey),		
+	} satisfies UnredactedConfig));
 });
